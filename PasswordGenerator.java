d1215cbc3a749ba745604b4f330efb9200f0d9d0
Sabina Smajlaj <ss3912@columbia.edu>
2014-09-29 15:29:04 -0400

    initial attempt

---
+++ PasswordGenerator.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class PasswordManager
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// get user input
+	}
+	
+	public static String generatePassword(String businessName, String businessState, String birthday){
+		
+	}
+}
